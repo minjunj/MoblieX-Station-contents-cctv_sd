28a8e762519761bd7435cf5c27f905c7df78c600
minjunj <minjun_jo@gm.gist.ac.kr>
2024-01-24 08:23:05 +0900

    add new logic that sent result data to client2

--- code/result/sub/sub-nats-minio-api.py
+++ code/result/sub/sub-nats-minio-api.py
@@ -39,8 +39,14 @@ async def download_from_minio(bucket_name, object_name, dest_file_name, retries=
                     file_name = "./out/"+"output"+str(count)+".png"  # Name of the output file
                     with open(file_name, "wb") as file:
                         file.write(response.content)  # Write binary content to the file
-            
-                
+                    async with httpx.AsyncClient() as client:
+                        try:
+                            files = {"init_image": (moto_file, response.content, "image/jpg")}
+                            response = await client.post(os.getenv('BG_API_PATH'), files=files, timeout=None)
+                            print("shot!")
+                        except:
+                            pass
+
         except S3Error as e:
             if e.code == "NoSuchKey" and attempt < retries - 1:
                 #print(f"Object {object_name} not found, retrying in {delay} seconds...")
